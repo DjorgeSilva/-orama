e1cdaafdadc83a2e945d4682d074ca91f6b665ac
Flânelon DJorge Silva Salazar <40239140+DjorgeSilva@users.noreply.github.com>
2021-01-18 11:59:26 -0300

    Add files via upload

--- src/components/Busca.js
+++ src/components/Busca.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import { AiOutlinePullRequest } from 'react-icons/ai';
 import "../css/index.css";
+import imgPerfilRisco from "../assets/img/perfil.png"
+
 
 export const Busca = () => {
+
+
+
+    function changeValueMinimo(){
+        var inputAplicacaoMinima = document.querySelector("#aplicacaoMinima");
+        var outputAplicacaoMinima = document.querySelector("#valueAplicacaoMinima");
+
+
+        outputAplicacaoMinima.innerHTML = inputAplicacaoMinima.value;
+
+        inputAplicacaoMinima.oninput = function() {
+            outputAplicacaoMinima.innerHTML = this.value;
+        }
+    }
+
+    function changeValuePrazoResgate(){
+        var inputPrazoResgate = document.querySelector("#prazoResgate");
+        var outputPrazoResgate = document.querySelector("#valuePrazoResgate");
+
+        outputPrazoResgate.innerHTML = inputPrazoResgate.value;
+
+
+        inputPrazoResgate.oninput = function() {
+            outputPrazoResgate.innerHTML = this.value;
+        }
+    }
+
+
+
     return (
         
         <div className="container-busca">
@@ -13,19 +45,20 @@ export const Busca = () => {
 
                     <div className="aplicacao-minima item-filtro">
                         <p>Aplicação mínima</p>
-                        <input type="range" name="aplicacao-minima" id=""/>
-                        <label htmlFor="aplicacao-minima">Até R$ 20.000,00</label>
+                        <input type="range" min="0" max="20000" id="aplicacaoMinima" onChange={changeValueMinimo}/>
+                        <label htmlFor="aplicacao-minima">Até R$ <span id="valueAplicacaoMinima"></span></label>
                     </div>
 
-                    <div className="aplicacao-minima item-filtro">
-                        <p>Perfil de risco de fundo</p>
-                        <input type="range" name="aplicacao-minima" id=""/>
+                    <div className="perfilRiscoFundo item-filtro perfilRisco">
+                        <p className="style">Perfil de risco de fundo</p>
+                        <img src={imgPerfilRisco} alt=""/>
+                        <input type="range" name="aplicacao-minima" id="perfilRisco"/>
                     </div>
 
-                    <div className="aplicacao-minima item-filtro">
-                        <p>Aplicação mínima</p>
-                        <input type="range" name="aplicacao-minima" id=""/>
-                        <label htmlFor="aplicacao-minima">Até R$ 20.000,00</label>
+                    <div className="prazoResgate item-filtro">
+                        <p>Prazo de resgate</p>
+                        <input type="range" min="0" max="30" id="prazoResgate" onChange={changeValuePrazoResgate}/>
+                        <label htmlFor="prazoResgate">Até <span id="valuePrazoResgate"></span> dias utéis</label>
                     </div>
                     
                 </div>
